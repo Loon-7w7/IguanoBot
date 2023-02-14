6145034fd6359715dc59c55b53e172a90ce7bfd5
Luis Ku <99504287+Loon-7w7@users.noreply.github.com>
2023-02-14 09:34:14 -0600

    first commit

---
+++ dist/Bot.js
@@ -0,0 +1,49 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const discord_js_1 = require("discord.js");
+const client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds,
+        "DirectMessages", "GuildPresences",
+        "MessageContent",
+        "DirectMessageReactions",
+        "GuildMessages",
+        "GuildMessageReactions",
+        "Guilds"] });
+const openai_1 = require("openai");
+const configAi = new openai_1.Configuration({
+    organization: process.env.OPENAI_ORGANIZATIO,
+    apiKey: process.env.OPENAI_TOKEN
+});
+const OpenAI = new openai_1.OpenAIApi(configAi);
+client.once('ready', c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+client.on("messageCreate", (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (msg.content.startsWith(process.env.Prefix + 'gpt')) {
+        try {
+            const menssge = msg.content.slice(6);
+            const gptResponse = yield OpenAI.createCompletion({
+                model: 'text-davinci-003',
+                prompt: `${menssge}`,
+                temperature: 0.9,
+                max_tokens: 1000,
+                stop: ['ChatGPT:', 'Tonanes:'],
+            });
+            msg.reply(`${gptResponse.data.choices[0].text}`);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+}));
+client.login(process.env.DISCORD_TOKEN);
---
+++ src/Bot.ts
@@ -0,0 +1,50 @@
+import {config} from 'dotenv'
+config();
+import {Client, GatewayIntentBits, Message} from 'discord.js'
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+    "DirectMessages","GuildPresences",
+    "MessageContent", 
+    "DirectMessageReactions", 
+    "GuildMessages", 
+    "GuildMessageReactions", 
+    "Guilds"] });
+
+import {Configuration, OpenAIApi} from 'openai';
+
+const configAi = new Configuration(
+	{
+		organization:process.env.OPENAI_ORGANIZATIO,
+		apiKey:process.env.OPENAI_TOKEN
+	});
+const OpenAI = new OpenAIApi(configAi);
+
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("messageCreate", async(msg) => { // When a message is created
+	if (msg.content.startsWith(process.env.Prefix + 'gpt'))
+	{
+		try
+			{
+				const menssge = msg.content.slice(6);
+				const gptResponse = await OpenAI.createCompletion(
+					{
+						model: 'text-davinci-003',
+						prompt: `${menssge}`,
+						temperature: 0.9,
+						max_tokens: 1000,
+						stop: ['ChatGPT:', 'Tonanes:'],
+					});
+				msg.reply(`${gptResponse.data.choices[0].text}`);
+			}
+			catch (e) 
+			{
+				console.error(e);
+			}
+	}
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
