83e6ae7ccf92d3f22d850dadbbc09e61e37593d8
Luis Ku <99504287+Loon-7w7@users.noreply.github.com>
2023-02-23 22:13:18 -0600

    complier

--- dist/Dicord objects/embledMessage.js
+++ dist/Dicord objects/embledMessage.js
@@ -11,7 +11,7 @@ function MessageGpt(discordMsg, messageBot) {
         .setTimestamp()
         .setFooter({ text: 'Adentro el Iguano', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
     if (discordMsg.author.avatar != null) {
-        MessageEmbed.setAuthor({ name: discordMsg.author.username, iconURL: discordMsg.author.avatar });
+        MessageEmbed.setAuthor({ name: discordMsg.author.username });
     }
     return MessageEmbed;
 }
